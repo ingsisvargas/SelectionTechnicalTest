085c691d0dcac43d85ef508365cb06ad2acb1220
ronavamo <ronaldvargas@seven4n.com>
2018-05-21 11:34:10 -0500

    Se agregan comentarios a los casos de prueba.

--- src/test/java/unitTest/PersonaResourceTest.java
+++ src/test/java/unitTest/PersonaResourceTest.java
@@ -23,8 +23,9 @@ import java.sql.Statement;
 import ar.com.mavha.selectionTechnicalTest.entities.Persona;
 import ar.com.mavha.selectionTechnicalTest.jaxrs.PersonaResource;
 
+
 public class PersonaResourceTest extends JerseyTest {
-    
+
 	@Override
 	protected Application configure() {
 		return new ResourceConfig(PersonaResource.class);
@@ -51,15 +52,14 @@ public class PersonaResourceTest extends JerseyTest {
 	 * @throws SQLException
 	 */
 	private static void initDatabase() throws SQLException {
-		
-		try ( Connection connection = getConnection(); Statement statement = connection.createStatement();) {
+
+		try (Connection connection = getConnection(); Statement statement = connection.createStatement();) {
 			statement.execute("CREATE TABLE Persona (dni VARCHAR(20) NOT NULL, nombre VARCHAR(50),"
 					+ "apellido VARCHAR(50), edad INT, PRIMARY KEY (dni))");
 			connection.commit();
 			connection.close();
-		} 
-		
-		
+		}
+
 	}
 
 	/**
@@ -72,7 +72,12 @@ public class PersonaResourceTest extends JerseyTest {
 		return DriverManager.getConnection("jdbc:hsqldb:mem:technicaltest", "sa", "");
 	}
 
-	
+	/**
+	 * Este caso de prueba valida que si no hay personas registradas el servicio
+	 * responde como tal.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void test1() throws Exception {
 		Response response = target("/personas").request().get();
@@ -81,40 +86,53 @@ public class PersonaResourceTest extends JerseyTest {
 		assertTrue(responseData.contains("No existen personas registradas en el sistema."));
 	}
 
+	/**
+	 * Este caso de prueba verifica que el servicio de ingresar una persona
+	 * funciona correctamente.
+	 * 
+	 * @throws Exception
+	 */
 	@Test
 	public void test2() throws Exception {
 		Persona persona = new Persona("1126425648", "Ronald", "Vargas", 25);
 		GenericType<String> returnType = new GenericType<String>() {
 		};
-		Response response = target("/personas").request()
-				.post(Entity.entity(persona,MediaType.APPLICATION_JSON));
+		Response response = target("/personas").request().post(Entity.entity(persona, MediaType.APPLICATION_JSON));
 
 		String responseData = response.readEntity(returnType);
 		assertEquals(200, response.getStatus());
 		assertTrue(responseData.contains("Se agregó la persona con éxito."));
 	}
-	
-	
 
+	/**
+	 * Este caso de prueba permite verificar que el servicio de ingresar personas valida
+	 * que la persona a ingresar no exista con el dni ingresado.
+	 * @throws Exception
+	 */
 	@Test
 	public void test3() throws Exception {
+
 		Persona persona = new Persona("1126425648", "Ronald", "Vargas", 25);
+
 		GenericType<String> returnType = new GenericType<String>() {
 		};
-		Response response = target("/personas").request()
-				.post(Entity.entity(persona,MediaType.APPLICATION_JSON));
+		
+		Response response = target("/personas").request().post(Entity.entity(persona, MediaType.APPLICATION_JSON));
 
 		String responseData = response.readEntity(returnType);
 		assertEquals(500, response.getStatus());
 		assertTrue(responseData.contains("Ya existe una persona registrada con el DNI ingresado."));
 
 	}
-	
 
+	/**
+	 * Este caso de prueba permite verificar que al listar las personas el servicio
+	 * funciona correctamente si hay personas en registradas en el sistema.
+	 */
 	@Test
-	public void test4() throws Exception {		
+	public void test4() throws Exception {
 		Response response2 = target("/personas").request().get();
-		String responseData2= response2.readEntity(String.class);
+		String responseData2 = response2.readEntity(String.class);
 		System.out.println(responseData2);
 		assertEquals(200, response2.getStatus());
 
